464d4724b1e0aecf5fa94c38d643dc0de20cac53
Vaishnavi Wakchaure <165935752+07Vaishnavi-Wakchaure@users.noreply.github.com>
2024-06-12 20:13:39 +0530

    Add files via upload

---
+++ code_1.py
@@ -0,0 +1,25 @@
+a="hello world"
+print(a)
+
+b='bobby \'s world'
+print(b)
+
+print(len(a))
+print(a[0])
+print(a[0:5])
+print(a[:5])
+print(a[6:])
+print(a.lower())
+print(a.upper())
+print(a.count('hello'))
+print(a.count('l'))
+print(a.find('world'))
+a="good morning"
+print(a)
+a_new=a.replace('morning','night')
+print(a_new)
+d="hi"
+e="hello"
+f=f'{d} {e}'
+print(f)
+print(dir(f))
